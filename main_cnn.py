fix: Fixed VGG (Gradient Vanishing)

main_cnn.py
@@ -1,7 +1,7 @@
 import torch
 import torch.optim as optim
 import torch.nn as nn
-from utils import create_img_loader
+from utils import create_img_loader, print_model_parameters
 from models import CNN_Base, Unet2, VGG16
 from scripts import train_model, eval_model_classification
 
@@ -25,6 +25,7 @@ def main():
                                      shuffle=True)
 
     # model을 불러옵니다.
+    # model = CNN_Base(3).to(device)
     model = VGG16(3).to(device)
 
     optimizer = optim.Adam(model.parameters(), lr=0.01)
@@ -41,7 +42,7 @@ def main():
                test_loader=test_loader,
                criterion=criterion,
                device=device)
-    
+
 
 if __name__ == "__main__":
     main()

models/Base_model.py
@@ -26,7 +26,7 @@ class CNN_Base(nn.Module):
         self.softmax = nn.Softmax(1)
 
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(128 * 24 * 24 ,100)
+        self.fc1 = nn.Linear(8192 ,100)
 
     def forward(self, input_tensor):
         # block 1

models/VGG.py
@@ -31,7 +31,7 @@ class VGG16(nn.Module):
 
         # Utils
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.LeakyReLU()
         self.flatten = nn.Flatten()
 
 

scripts/eval.py
@@ -1,5 +1,6 @@
 import torch
 from tqdm import tqdm
+from utils import debug
 
 def eval_model_regression(model, test_loader, criterion, device):
     '''
@@ -52,7 +53,6 @@ def eval_model_classification(model, test_loader, criterion, device):
             loss = criterion(outputs,labels)
 
             evaluation_loss += loss.item() * inputs.size(0)
-
             _, predicted = torch.max(outputs, 1)
             correct += (labels==predicted).sum().item()
 